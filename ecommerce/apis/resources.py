14870f586edb665a02a36ab3902e8eba2b5cc94f
AravindR97 <aravindrajeshchry@gmail.com>
2025-09-27 11:12:45 +0530

    feat(API): Add api to fetch Image Set for ads

---
+++ ecommerce/apis/resources.py
@@ -0,0 +1,23 @@
+import frappe
+
+@frappe.whitelist(allow_guest=True)
+def get_image_set(set_name):
+    """
+    Fetch images from an Image Set and return their public URLs
+    """
+    image_set = frappe.get_doc("Image Set", set_name)
+
+    result = {
+        "name": image_set.name,
+        "images": []
+    }
+
+    for item in image_set.images:
+        result["images"].append({
+            "image_name": item.image_name if item.image_name else "",
+            "image_url": frappe.utils.get_url(item.image) if item.image else None
+        })
+
+    frappe.local.response.update(result)
+
+    return
